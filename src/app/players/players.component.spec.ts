c26fd1d188a785f98fb44fe49131b4e77fe823ca
Matt Smith <matt.d.smith.89@gmail.com>
2018-11-25 20:05:13 +0000

    Fix test

--- src/app/players/players.component.spec.ts
+++ src/app/players/players.component.spec.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { Overview } from '../models/overview';
 import { DataService } from '../data.service';
 import { FormsModule } from '@angular/forms';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 class MockDataService {
   get data(): Observable<Overview> {
@@ -19,7 +20,7 @@ describe('PlayersComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PlayersComponent],
-      imports: [FormsModule],
+      imports: [FormsModule, ScrollingModule],
       providers: [{ provide: DataService, useClass: MockDataService }]
     })
       .compileComponents();
